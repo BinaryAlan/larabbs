13af2d5c35d6e24847ace8c7d6ef413a081f7ac3
LiaoChao <76660421@qq.com>
2018-07-20 09:24:43 +0000

    WYSIWYG edit

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,6 +7,7 @@
     <meta name="csrf-token" content="{{csrf_token()}}">
 
     <title>@yield('title','LaraBBS') - Larabel 进阶教程</title>
+    @yield('styles')
     <link rel=stylesheet href="{{asset('css/app.css')}}">
 </head>
 <body>
@@ -19,5 +20,6 @@
         @include('layouts._footer')
     </div>
     <script src="{{asset('js/app.js')}}"></script>
+    @yield('scripts')
 </body>
 </html>
