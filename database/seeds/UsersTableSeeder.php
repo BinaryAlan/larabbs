03bf9c1972b7a42c3ab17c5242600b05531c0816
LiaoChao <76660421@qq.com>
2018-07-23 03:58:15 +0000

    integrate laravel-permission package

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -36,5 +36,12 @@ class UsersTableSeeder extends Seeder
         $user->avatar   = "http://larabbs.test/uploads/images/avatars/201807/19/1_1531967929_s6H1UsmiQg.png";
         $user->save();
 
+        // 初始化用户角色，将 1 号用户指派为『站长』
+        $user->assignRole('Founder');
+
+        // 将 2 号用户指派为『管理员』
+        $user = User::find(2);
+        $user->assignRole('Maintainer');
+
     }
 }
