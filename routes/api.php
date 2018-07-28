cd479fee039850e489bda83c0a834650029a3e4d
LiaoChao <76660421@qq.com>
2018-07-28 13:14:02 +0000

    api/user

--- routes/api.php
+++ routes/api.php
@@ -13,7 +13,8 @@
 $api = app('Dingo\Api\Routing\Router');
 
 $api->version('v1', [
-    'namespace' => 'App\Http\Controllers\Api',
+    'namespace'  => 'App\Http\Controllers\Api',
+    'middleware' => 'serializer:array',
 ], function ($api) {
     $api->group([
         'middleware' => 'api.throttle',
@@ -41,5 +42,11 @@ $api->version('v1', [
 
         $api->delete('authorizations/current', 'AuthorizationsController@destroy')
             ->name('api.authorizations.destroy');
+
+        $api->group(['middleware' => 'api.auth'], function ($api) {
+            // 当前登录用户信息
+            $api->get('user', 'UsersController@me')
+                ->name('api.user.show');
+        });
     });
 });
