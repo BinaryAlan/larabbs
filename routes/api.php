0f76db3e3213c10b87e6ab260c7a7a3842617162
LiaoChao <76660421@qq.com>
2018-07-29 09:08:30 +0000

    replies index

--- routes/api.php
+++ routes/api.php
@@ -55,6 +55,12 @@ $api->version('v1', [
         $api->get('topics/{topic}', 'TopicsController@show')
             ->name('api.topics.show');
 
+        $api->get('topics/{topic}/replies', 'RepliesController@index')
+            ->name('api.topics.replies.index');
+
+        $api->get('users/{user}/replies', 'RepliesController@userIndex')
+            ->name('api.users.replies.index');
+
         $api->group(['middleware' => 'api.auth'], function ($api) {
             // 当前登录用户信息
             $api->get('user', 'UsersController@me')
