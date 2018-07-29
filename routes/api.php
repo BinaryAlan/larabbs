8b122ade841639a570cfee1c119552d9d173b23e
LiaoChao <76660421@qq.com>
2018-07-29 12:33:41 +0000

    permission index

--- routes/api.php
+++ routes/api.php
@@ -96,6 +96,9 @@ $api->version('v1', [
             $api->patch('user/read/notifications', 'NotificationsController@read')
                 ->name('api.user.notifications.read');
 
+            $api->get('user/permissions', 'PermissionsController@index')
+                ->name('api.user.permissions.index');
+
         });
     });
 });
