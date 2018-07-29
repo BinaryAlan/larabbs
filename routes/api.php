421db4d44ed8de1e5782d3da9140415306660cda
LiaoChao <76660421@qq.com>
2018-07-29 09:22:37 +0000

    notifications stats

--- routes/api.php
+++ routes/api.php
@@ -90,6 +90,9 @@ $api->version('v1', [
             $api->get('user/notifications', 'NotificationsController@index')
                 ->name('api.user.notifications.index');
 
+            $api->get('user/notifications/stats', 'NotificationsController@stats')
+                ->name('api.user.notifications.stats');
+
         });
     });
 });
